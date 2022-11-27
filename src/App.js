a29ebc6f9957a3579e74ce67e8ebf5cb5b6fe76f
Aisha Gul <aishagulahmed@gmail.com>
2022-11-28 00:28:50 +0500

    use effect example & explanation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState , useEffect} from 'react' //need to import hooks for using them
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+  const [count, setCount] = useState(0);  // useState hook 
+
+  useEffect(() => {                       // use effect hook 
+    setTimeout(() => {                    // javascript function 
+      setCount((count) => count + 1);     // javascipt function to increment the count variable by 1 
+    }, 1000);                             // here we pass second argument which tells timeout function about how much time this function has to run in current senerio it'll run after 1sec(1000 millisecond)
+  }, []);                                 // empty brackets here tell the useEffect hook to run only once. 
+
+  return <h1>I've rendered {count} times!</h1>; //the count value 'll be 0 initially but after 1sec it'll update as 1
 }
 
-export default App;
+export default App
